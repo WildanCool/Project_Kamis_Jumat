ef1118d584e6634651935e4f28b809358ab61f9e
Perdana Muhammad Wildanumukhaladun <160695487+WildanCool@users.noreply.github.com>
2026-04-15 11:24:17 +0700

    updt
    belajar API

--- project_kamis_jumat/lib/belajar_API/coba.dart
+++ project_kamis_jumat/lib/belajar_API/coba.dart
@@ -14,16 +14,17 @@ class _CobaAPIState extends State<CobaAPI> {
   bool isLoading = true;
 
   Future ambilData() async {
-    var response = await http.get(
-      Uri.parse('https://jsonplaceholder.typicode.com/users'),
-    );
+    var response = await http.get(Uri.parse('https://dummyjson.com/products'));
 
-    var hasil = jsonDecode(response.body);
-
-    setState(() {
-      data = hasil;
-      isLoading = false;
-    });
+    if (response.statusCode == 200) {
+      var hasil = jsonDecode(response.body);
+      setState(() {
+        data = hasil['products'];
+        isLoading = false;
+      });
+    } else {
+      throw Exception('Gagal mengambil data user');
+    }
   }
 
   @override
@@ -39,8 +40,64 @@ class _CobaAPIState extends State<CobaAPI> {
       body: isLoading
           ? const Center(child: CircularProgressIndicator())
           : ListView(
-              children: data.map((item) {
-                return ListTile(title: Text(item['email']));
+              padding: EdgeInsets.all(10),
+              children: data.take(5).map((item) {
+                // return Container(
+                //   margin: EdgeInsets.only(bottom: 12),
+                //   child: Card(
+                //     elevation: 4,
+                //     shape: RoundedRectangleBorder(
+                //       borderRadius: BorderRadius.circular(12),
+                //     ),
+                //     child: Padding(
+                //       padding: EdgeInsets.all(16),
+                //       child: Row(
+                //         children: [
+                //           CircleAvatar(radius: 25, child: Icon(Icons.person)),
+
+                //           SizedBox(width: 12),
+
+                //           Column(
+                //             crossAxisAlignment: CrossAxisAlignment.start,
+                //             children: [
+                //               Text(
+                //                 item['name'],
+                //                 style: TextStyle(
+                //                   fontSize: 16,
+                //                   fontWeight: FontWeight.bold,
+                //                 ),
+                //               ),
+                //               SizedBox(height: 5),
+                //               Text(
+                //                 item['email'],
+                //                 style: TextStyle(color: Colors.grey),
+                //               ),
+                //             ],
+                //           ),
+                //         ],
+                //       ),
+                //     ),
+                //   ),
+                // );
+                return Container(
+                  // height: 200,
+                  width: 200,
+                  color: Colors.grey.shade100,
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Image.network(item['images'][0], height: 150),
+                      Text(
+                        item['title'],
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        item['description'],
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                );
               }).toList(),
             ),
     );
